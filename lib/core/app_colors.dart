c3bc6bc3c69349b9373b6d6cd16cb1a6a0682286
agresoth0117-stack <agresoth.0117@gmail.com>
2025-12-10 05:10:53 -0500

    Delete lib directory

--- lib/core/app_colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const background = Color.fromARGB(255, 221, 221, 221);
-  static const primary = Color(0xFF1E88E5);
-  static const card = Color.fromARGB(255, 1, 28, 46);
-}
